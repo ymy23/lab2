a419f6b66e800ca986359b883359fe7fdfaf121a
ymy23 <99948628+ymy23@users.noreply.github.com>
2022-12-20 11:44:10 +0800

    Add files via upload

---
+++ xv6-lab3/xv6-labs-2022/kernel/sysproc.c
@@ -0,0 +1,118 @@
+#include "types.h"
+#include "riscv.h"
+#include "param.h"
+#include "defs.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  argint(0, &n);
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  argaddr(0, &p);
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  uint64 addr;
+  int n;
+
+  argint(0, &n);
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+
+  argint(0, &n);
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(killed(myproc())){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+
+#ifdef LAB_PGTBL
+int
+sys_pgaccess(void)
+{
+  // lab pgtbl: your code here.
+  int len;
+  uint64 addr;
+  int bitmask;
+  argaddr(0,&addr);
+  argint(1,&len);
+  argint(2,&bitmask);
+  struct proc *p = myproc();
+  int res = 0x0;
+  if(len>32||len<0) return -1;
+  for(int i = 0;i<len;i++){
+    int va = addr + i *PGSIZE;
+    int abit = vmpgaccess(p->pagetable,va);
+    res = res|abit<<i;
+  }
+  if(copyout(p->pagetable, bitmask, (char*)&res, sizeof(res)) < 0)
+      return -1;
+  return 0;
+}
+#endif
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  argint(0, &pid);
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
